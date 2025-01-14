64125d38abbee8aece815ecd92e844a2b228e104
Henrique Miranda <hmiranda@ciandt.com>
2025-01-14 18:42:11 -0300

    Improving sizes and spacings

--- androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
+++ androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
@@ -14,15 +14,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import coil3.compose.AsyncImage
 
@@ -39,9 +44,10 @@ class MainActivity : ComponentActivity() {
 @Preview
 fun HomeScreen() {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.spacedBy(48.dp, Alignment.CenterVertically),
         modifier = Modifier
             .background(color = Color(0xFF1F1F1F))
+            .padding(horizontal = 32.dp)
             .fillMaxSize()
     ) {
         Section("Section 1",
@@ -63,16 +69,21 @@ data class SectionContent(
 )
 
 @Composable
-fun Section(header: String, contents: List<SectionContent>) {
-    Column(Modifier.wrapContentSize()) {
-        BasicText(color = { Color.White }, text = header)
+fun Section(header: String, contents: List<SectionContent>, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterVertically),
+        modifier = modifier
+    ) {
+        BasicText(style = TextStyle(color = Color.White, fontSize = 32.sp), text = header)
+
         LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
             items(contents) {
                 AsyncImage(
                     contentDescription = it.accessibilityDescription,
+                    contentScale = ContentScale.FillBounds,
                     model = it.imageModel,
                     placeholder = painterResource(R.drawable.placeholder_loading),
-                    modifier = Modifier.wrapContentSize(),
+                    modifier = Modifier.requiredSize(width = 200.dp, height = 300.dp)
                 )
             }
         }

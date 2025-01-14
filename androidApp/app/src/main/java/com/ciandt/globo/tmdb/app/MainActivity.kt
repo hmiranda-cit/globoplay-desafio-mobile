133fa2e2d5bb77603e2d4aae329f1457ee39bffc
Henrique Miranda <hmiranda@ciandt.com>
2025-01-14 18:56:34 -0300

    Improve rendering

--- androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
+++ androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
@@ -43,23 +44,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun HomeScreen() {
-    Column(
+    LazyColumn(
         verticalArrangement = Arrangement.spacedBy(48.dp, Alignment.CenterVertically),
         modifier = Modifier
             .background(color = Color(0xFF1F1F1F))
             .padding(horizontal = 32.dp)
             .fillMaxSize()
     ) {
-        Section("Section 1",
-            listOf(
-                SectionContent("Mickey", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7oaspVjLS8F-NDM-C7m8YJHX1eSPlcvw197CMD7ryqhioXalaoOOb4-Z-lyQDqzUrouM&usqp=CAU"),
-                SectionContent("Tintin", "https://bleedingcool.com/wp-content/uploads/2025/01/084fd4b8-5923-40d5-bfb5-7301d0b47221.jpeg"),
-                SectionContent("Popeye", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQduJlbk6z6w-5aZqbWl21_PgRBMlH0_euTag&s"),
-                SectionContent("Zé Carioca", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHaJGZ3PcpUwQ2eXiI62mtjnHb-hfDafwhQ&s"),
+        item {
+            Section(
+                "Comics",
+                listOf(
+                    SectionContent("Mickey", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7oaspVjLS8F-NDM-C7m8YJHX1eSPlcvw197CMD7ryqhioXalaoOOb4-Z-lyQDqzUrouM&usqp=CAU"),
+                    SectionContent("Tintin", "https://bleedingcool.com/wp-content/uploads/2025/01/084fd4b8-5923-40d5-bfb5-7301d0b47221.jpeg"),
+                    SectionContent("Popeye", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQduJlbk6z6w-5aZqbWl21_PgRBMlH0_euTag&s"),
+                    SectionContent("Zé Carioca", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHaJGZ3PcpUwQ2eXiI62mtjnHb-hfDafwhQ&s"),
+                )
             )
-        )
-        Section("Section 2", emptyList())
-        Section("Section 3", emptyList())
+        }
+        item {
+            Section(
+                "Movies",
+                listOf(
+                    SectionContent("Cinderela", "https://upload.wikimedia.org/wikipedia/pt/4/47/Cinderela_Baiana.jpg"),
+                    SectionContent("Darko", "https://upload.wikimedia.org/wikipedia/pt/thumb/5/58/Donnie_Darko.jpg/220px-Donnie_Darko.jpg"),
+                    SectionContent("Godfather", "https://upload.wikimedia.org/wikipedia/pt/a/af/The_Godfather%2C_The_Game.jpg"),
+                    SectionContent("Juno", "https://upload.wikimedia.org/wikipedia/pt/thumb/6/6b/Juno_P%C3%B4ster.jpg/220px-Juno_P%C3%B4ster.jpg"),
+                )
+            )
+        }
+        item {
+            Section(
+                "Books",
+                listOf(
+                    SectionContent("Casmurro", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dom_Casmurro.djvu/page1-369px-Dom_Casmurro.djvu.jpg"),
+                    SectionContent("AM", "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/IHaveNoMouth.jpg/220px-IHaveNoMouth.jpg"),
+                    SectionContent("Orbis", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Esmeraldo_de_situ_orbis%2C_1892.JPG/800px-Esmeraldo_de_situ_orbis%2C_1892.JPG"),
+                    SectionContent("Juno", "https://upload.wikimedia.org/wikipedia/en/c/cb/The_Chronicles_of_Narnia_box_set_cover.jpg"),
+                )
+            )
+        }
     }
 }
 

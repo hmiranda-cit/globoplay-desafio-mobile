7792aa668f17741c63ecfc5ff9910a7bb4d87c46
Henrique Miranda <hmiranda@ciandt.com>
2025-01-15 08:19:00 -0300

    Add bottom bar to home screen

--- androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
+++ androidApp/app/src/main/java/com/ciandt/globo/tmdb/app/MainActivity.kt
@@ -9,10 +9,14 @@ package com.ciandt.globo.tmdb.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -20,6 +24,10 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -27,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,12 +55,74 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun HomeScreen() {
+    val referenceScreenHeightPx = 1280f
+    val referenceBottomBarHeightPx = 112f
+
     Scaffold(
-        bottomBar = {},
+        bottomBar = {
+            BottomAppBar(
+                Modifier
+                    .fillMaxHeight(referenceBottomBarHeightPx / referenceScreenHeightPx)
+                    .fillMaxWidth()
+            )
+        },
         topBar = {},
         floatingActionButton = {},
     ) { innerPadding ->
         MovieList(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BottomAppBar(modifier: Modifier = Modifier) {
+    BottomAppBar(
+        backgroundColor = Color.Black,
+        modifier = Modifier
+            .then(modifier)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            BottomAppBarButton(
+                iconRes = R.drawable.baseline_home_24,
+                onClick = {},
+                text = stringResource(R.string.home_screen_bottombar_button_text),
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomAppBarButton(
+    @DrawableRes iconRes: Int,
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Button(
+        colors = buttonColors(backgroundColor = Color.Transparent),
+        onClick = onClick,
+        modifier = Modifier
+            .then(modifier)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+            modifier = Modifier
+        ) {
+            val sharedModifier = Modifier.align(Alignment.CenterHorizontally)
+            Icon(
+                tint = Color.White,
+                contentDescription = null,
+                painter = painterResource(iconRes),
+                modifier = sharedModifier
+            )
+            BasicText(
+                style = TextStyle(color = Color.White, fontSize = 16.sp),
+                text = text,
+                modifier = sharedModifier
+            )
+        }
     }
 }
 
